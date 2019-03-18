fb395615f113bc6673eb9eaa28ba4f20cf7c32b4
Anton Ivanov-Telmanov <aivtel@gmail.com>
2019-03-18 19:43:53 +0300

    improving some changes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
 
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-             return ( 
-             <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                </li>
-        );});
+    componentWillUpdate() {
+        console.log("Order summary will update")
+    }
 
-    return (
-        <React.Fragment>
-            <h3>Your order:</h3>
-            <p>A delicious burger with following ingredients:</p>
-            <ul>
-`               {ingredientSummary}
-            </ul>
-            <p><strong>Total price: {props.price.toFixed(2)} $</strong></p>
-            <p>Continue to check out?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
-        </React.Fragment>
-    )
+    render() {
+
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return ( 
+            <li key={igKey}>
+                   <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+               </li>
+       );});
+
+        return (
+            <React.Fragment>
+                <h3>Your order:</h3>
+                <p>A delicious burger with following ingredients:</p>
+                <ul>
+    `               {ingredientSummary}
+                </ul>
+                <p><strong>Total price: {this.props.price.toFixed(2)} $</strong></p>
+                <p>Continue to check out?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+            </React.Fragment>
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-    return (
-        <React.Fragment>
-            <Backdrop show={props.show} clicked={props.modalClosed}/>
-            <div className={classes.Modal}
-                style={{
-                        transform: props.show ? 'translateY(0)' : 'translateY(-200vh)',
-                        opacity: props.show ? '1' : '0'
-                    }}
-                >
-            {props.children}
-            </div>
-        </React.Fragment>
-    )
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if(nextProps.show !== this.props.show) {
+            return true;
+        }
+        return false;
+    }
+
+
+    componentWillUpdate() {
+        console.log('Modal will update')
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal}
+                    style={{
+                            transform: this.props.show ? 'translateY(0)' : 'translateY(-200vh)',
+                            opacity: this.props.show ? '1' : '0'
+                        }}
+                    >
+                {this.props.children}
+                </div>
+            </React.Fragment>
+        )
+    }
 }
 
-export default modal;
+export default Modal;
