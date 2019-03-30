c0e7725882d2976f1cf92b6f22db116a25d2438a
Anton Ivanov-Telmanov <aivtel@gmail.com>
2019-03-30 16:14:24 +0300

    checkout change show

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,6 +5,10 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
+    
+    state = {
+        showing: true
+    };
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
@@ -12,19 +16,23 @@ class Checkout extends Component {
 
     checkoutContinuedHandler = () => {
         this.props.history.replace('/checkout/contact-data');
+        this.setState({
+            ...this.state,
+            showing: false});
     }
 
     render() {
         let summary = <Redirect to="/" />
         if (this.props.ings) {
             const purchasedRedirect = this.props.purchased ? <Redirect to='/' /> : null;
+            const checkoutSummary = this.state.showing ? <CheckoutSummary 
+            checkoutCancelled={this.checkoutCancelledHandler} 
+            checkoutContinued={this.checkoutContinuedHandler} 
+            ingredients={this.props.ings} /> : null;
             summary = (
                 <div>
                     {purchasedRedirect}
-                    <CheckoutSummary 
-                        checkoutCancelled={this.checkoutCancelledHandler} 
-                        checkoutContinued={this.checkoutContinuedHandler} 
-                        ingredients={this.props.ings} />
+                    {checkoutSummary}
                     <Route 
                         path={this.props.match.path + '/contact-data'} 
                         component={ContactData} />
